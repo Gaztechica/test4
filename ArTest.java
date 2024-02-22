817a1387b7519666ecd7ab404717c24a446235b1
Evgeniy <119495875+Gaztechica@users.noreply.github.com>
2024-02-23 00:13:03 +0400

    Update ArTest.java

--- ArTest.java
+++ ArTest.java
@@ -1,1 +1,8 @@
+WebElement submitButtonlProject = driver.findElement(By.xpath("//button[@class='ant-btn ant-btn-default primaryButton big colorPrimary ']"));
 
+            submitButtonlProject.click();
+
+
+            WebElement textProjectName = driver.findElement(By.xpath("//input[@class='ant-input primaryInput  not-entered']"));
+
+            textProjectName.sendKeys("1Новый проект");
