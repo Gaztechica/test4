6a6ae30c85f45fe398aca7093027963dca9c9aae
Tihhonix <tihhonix@gmail.com>
2024-03-10 16:39:37 +0400

    Merge remote-tracking branch 'origin/cv' into cv

--- src/test/java/ar/runner/BaseTest.java
+++ src/test/java/ar/runner/BaseTest.java
@@ -21,7 +21,7 @@ import java.time.Duration;
         public static final String INPUT_EMAIL = "//input[@class='ant-input primaryInput  not-entered']";
         public static final String BTN_PASSWORD = "//button[@class='ant-btn ant-btn-default authButton big colorPrimary ']";
         public static final String EMAIL = "tmzejehj@happy9toy.com";
-        public static final String PASSWORD = "";
+        public static final String PASSWORD = "45522123658";
 
         //    public static final String DESCRIPTION = "Test description";
 
